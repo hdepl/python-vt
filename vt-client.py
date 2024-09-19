6b2b5b1f18f19c5fab5dfd6556b092f139866e83
hdepl <64508546+hdepl@users.noreply.github.com>
2024-09-19 17:19:45 +0530

    Update vt-client.py

--- vt-client.py
+++ vt-client.py
@@ -1,2 +1,14 @@
 #Virtual Trading Application - Version 1.0
 
+import mysql.connector
+import time
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
