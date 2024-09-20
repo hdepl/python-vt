c2259252d04d507a6508e2219b6938e69c97cac3
hdepl <64508546+hdepl@users.noreply.github.com>
2024-09-20 07:08:12 +0530

    Update vt-client.py

--- vt-client.py
+++ vt-client.py
@@ -1,14 +1,16 @@
 #Virtual Trading Application - Version 1.0
 
-import mysql.connector
-import time
-
+import mysql.connector    #connector for MySQL
+import time               #time library module 
+import math               #math library module 
+#database credentials will be changed once after completion of the project
 mydb = mysql.connector.connect(
   host="localhost",
   user="yourusername",
   password="yourpassword",
   database="mydatabase"
 )
-
+#cursor object for running the sql querries
 mycursor = mydb.cursor()
+
 
