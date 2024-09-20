b2fd94d31070197891335a107bf86d0ed068631a
hdepl <64508546+hdepl@users.noreply.github.com>
2024-09-20 07:26:22 +0530

    Update vt-client.py

--- vt-client.py
+++ vt-client.py
@@ -5,10 +5,10 @@ import time               #time library module
 import math               #math library module 
 #database credentials will be changed once after completion of the project
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="yourusername",
-  password="yourpassword",
-  database="mydatabase"
+  host="103.211.216.137",
+  user="kivaabip_stocks",
+  password="hdepl@2020",
+  database="kivaabip_stockmarket"
 )
 #cursor object for running the sql querries
 mycursor = mydb.cursor()
@@ -16,5 +16,6 @@ mycursor = mydb.cursor()
 username = int(input("Enter your Mobile Number : "))
 password = input("Enter the password : ")
 
-print("Username : ", username)
-print("Password : ", password)
+#print("Username : ", username)
+#print("Password : ", password)
+
